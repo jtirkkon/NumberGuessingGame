515e7f6b0f67767d43a9e30e0d1c6117dc430976
jtirkkon <jouni.tirkkonen@gmail.com>
2021-01-26 12:04:39 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [quess, setQuess] = useState('');
+  const [infoText, setInfoText] = useState('Guess a number between 1-100');
+  const [quesses, setQuesses] = useState(1);
+  const [secretNumber, setSecretNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  
+  const checkQuess = () => {
+    setQuesses(quesses + 1);
+    if (parseInt(quess) < secretNumber) {
+      setInfoText(`Your guess ${quess} is too low`);
+    } else if(parseInt(quess) > secretNumber) {
+      setInfoText(`Your guess ${quess} is too high`);  
+    } else {
+      Alert.alert(`You guessed the number in ${quesses} quesses`);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{infoText}</Text>
+      <TextInput style={styles.textInputStyle} onChangeText={text => setQuess(text)} value={quess} keyboardType="number-pad"></TextInput>
+      <Button title="MAKE QUESS" onPress={checkQuess}></Button>
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +36,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textInputStyle: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 30,
+    marginBottom: 30
   },
 });
